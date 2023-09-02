d6900936b9aa03120f906cea300892ef4dd62a39
Vincent VIEIRA <vincent.vieira.carbon-it@ext.pmu.fr>
2023-09-02 13:33:53 +0200

    adding kotlin support

--- fizz-buzz-rest-api/build.gradle.kts
+++ fizz-buzz-rest-api/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     java
     id("org.springframework.boot") version "3.1.3"
+    kotlin("jvm") version "1.9.10"
 }
 
 apply(plugin = "io.spring.dependency-management")
@@ -8,6 +9,7 @@ apply(plugin = "io.spring.dependency-management")
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("com.ninja-squad:springmockk:4.0.2")
 }
 
 tasks.test {
